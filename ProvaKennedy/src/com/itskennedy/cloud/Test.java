a18d5864546e7dbeda99dcb32805099314a8228f
ITSPediciniA <ITSPediciniA@itscor207358.itsacademy.corvallisholding.it>
2021-03-03 10:12:25 +0100

    terzo commento

--- ProvaKennedy/src/com/itskennedy/cloud/Test.java
+++ ProvaKennedy/src/com/itskennedy/cloud/Test.java
@@ -3,8 +3,11 @@ package com.itskennedy.cloud;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Ciao a tutti");
+		test();
 
 	}
 
+	public static void test() {
+		
+	}
 }
